1d13fbc66434c59180c59721185035b932273ea4
Ganiat Badara <ganiyatbadara@gmail.com>
2022-03-23 19:38:52 +0000

    Create a function to convert input to lower case

--- run.py
+++ run.py
@@ -25,3 +25,30 @@ def check_input_type(text):
         print('Invalid input. Please enter alphabets')
     else:
         return True
+
+def convert_to_lowercase(value):
+    # Convert input to lower case. 
+    return value.lower()
+
+
+
+def initialize_app():
+    initialize = True
+    while initialize:
+        print('Welcome to the Budget App!\n')
+        print('Please enter "All" to display all recorded income and expenses.\n')
+        print('Please enter "Income" to display all recorded income.\n')
+        print('Please enter "Expenses" to display all recorded expenses.\n')
+        print('Please enter "Update" to make changes to the budget.\n')
+        print('Please enter "Highest" to get the your highest expenses.\n')
+        print('Enter "clear" to clear worksheet.\n')
+        print('To end the process please enter "Exit".\n')
+      
+        user_input = input('Enter your data here: ')
+        'Validate input type'
+
+        if check_input_type(user_input) == True:
+            check_entered_values(convert_to_lowercase(user_input))
+
+
+initialize_app()
